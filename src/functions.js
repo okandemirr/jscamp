b2dfc50f34d835d01e6222382462427b467f3bba
Okan Demir <76594362+okandemirr@users.noreply.github.com>
2021-11-30 17:28:16 +0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,26 +1,4 @@
 
-// let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
-//     {name:"İç Anadolu", population:"20M"},
-//     {name:"Marmara", population:"30M"},
-//     {name:"Karadeniz", population:"10M"},
-//     [
-//         ["Ankara","Konya"],
-//         ["İstanbul","Bursa"],
-//         ["Sinop","Trabzon"],
-//     ]
-// ]
-
-// // console.log(icAnadolu.name)
-// // console.log(icAnadolu.population)
-// console.log(icAnadoluSehirleri)
-
-// let newProductName, newUnitPrice, newQuantity
-// ({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-// = {productName:"Elma", unitPrice:10, quantity:5})
-
-// console.log(newProductName)
-// console.log(newUnitPrice)
-// console.log(newQuantity)
 
 function adToCart(quantity,productName="Elma") {
     console.log("Sepete Eklendi : " + productName + " adet : " +quantity)
@@ -30,7 +8,7 @@ adToCart(10);
 // adToCart(15);
 
 let sayHello = () =>{
-    console.log("Hello B*tch!")
+    console.log("Hello")
 }
 sayHello();
 
